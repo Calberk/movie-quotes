0066a77e0f490ccf7cd75c96f1f50365ca1fccdf
Calberk <park.edmund@gmail.com>
2018-11-28 15:12:55 -0800

    added app file with routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize';
+import About from './about';
+import Home from './home';
+import Nav from './nav';
+import PublicList from './public_list';
+import SecretList from './secret_list';
+import Quotes from './quotes';
+import SignIn from './sign_in';
+import SignUp from './sign_up';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        
     </div>
 );
 
