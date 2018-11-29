adcf32a43b5adaa519dd365d188f9f7bce68df4d
Calberk <park.edmund@gmail.com>
2018-11-29 11:34:06 -0800

    added user authorization

--- src/components/secret_list.js
+++ src/components/secret_list.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import auth from '../hoc/auth';
 
-export default props => (
+const SecretList = props => {
+    
+    
+    return(
     <div>
         <h1 className="center">Secret Operatives List</h1>
         <ol>
             <li>Jim</li>
-            <li>Erick</li>
+    <li>Erick</li>
             <li>Collin</li>
             <li>Erin</li>
             <li>Scott</li>
         </ol>
     </div>
-)
+    )
+}
+
+export default auth(SecretList)
